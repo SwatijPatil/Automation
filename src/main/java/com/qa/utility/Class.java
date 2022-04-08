481e20fdafeb63ef8d7a02d3560ffb9f84baf929
SwatijPatil <swatip7878@gmail.com>
2022-04-08 14:03:45 +0530

    first commit

---
+++ src/main/java/com/qa/utility/Class.java
@@ -0,0 +1,11 @@
+package com.qa.utility;
+
+import com.qa.base.Testbase;
+import org.openqa.selenium.support.ui.Select;
+
+public abstract class Class extends Testbase {
+
+public abstract void societyDropdown();
+
+
+}
